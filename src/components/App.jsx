e9336626b65985611f76da9916c7e531a06f8e1d
SBurnt <zip2004zip@gmail.com>
2019-01-26 20:46:52 +0300

    feat: add site structure

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import '../css/App.css';
+
+import Header from './Header';
+import Home from './Home';
+import PhotographerList from './PhotographerList';
+import PhotographerInfo from './PhotographerInfo';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <main className='main'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/photographers' component={PhotographerList} />
+            <Route path='/photographers/:photographer' component={PhotographerInfo} />
+          </Switch>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
