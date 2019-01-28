013470c98ee7b8560d61ca7d17a968a68905d21d
SBurnt <zip2004zip@gmail.com>
2019-01-28 23:45:08 +0300

    style: add style PhotographerList

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,6 +3,8 @@ import Reflux from 'reflux';
 import actions from '../actions';
 import Store from '../Store';
 
+import '../css/PhotographerList.css';
+
 class Searchbar extends Reflux.Component {
   constructor(props) {
     super(props);
@@ -12,20 +14,19 @@ class Searchbar extends Reflux.Component {
 
   render() {
     const { language, isLoaded } = this.state;
-    
     if (isLoaded) {
       const lastName = language === 'bel' ? 'Прозвішча' : language === 'rus' ? 'Фамилия' : 'Last name';
       const button = language === 'bel' ? 'Шукаць' : language === 'rus' ? 'Искать' : 'Search'; 
       return (
-        <form onSubmit={actions.handleSubmit}>
-          <input type='text' value={this.state.input} placeholder={lastName} onChange={actions.changeInput} />
-          <button type='submit'>{button}</button>
+        <form className='formSearchForogr' onSubmit={actions.handleSubmit}>
+          <input className='inputSearchForogr' type='text' value={this.state.input} placeholder={lastName} onChange={actions.changeInput} />
+          <button className='btnSearchForogr' type='submit'>{button}</button>
         </form>
       );
     }
     return (
       <p>Loading...</p>
-    )
+    );
   }
 }
 
