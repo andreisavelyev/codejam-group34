b099f45c4c9fe84c041ad0316821e1ee5f5c71c6
Andrei Savelyeu <raikaat@mail.ru>
2019-01-29 00:00:17 +0300

    Updates

---
+++ precache-manifest.a7954506d5140559fd440927c3326f41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1346e5626923a5c35e9a877512728f27",
+    "url": "/codejam-group34/static/media/avatorSerg.1346e562.jpg"
+  },
+  {
+    "revision": "98257a7764d38df7b1907bb374017de6",
+    "url": "/codejam-group34/static/media/avatorIlyay.98257a77.jpg"
+  },
+  {
+    "revision": "bc4e9dd036a339df39b90bf2db6dba55",
+    "url": "/codejam-group34/static/media/avatorAndr.bc4e9dd0.jpg"
+  },
+  {
+    "revision": "47777b527659df7b84d4",
+    "url": "/codejam-group34/static/js/runtime~main.47777b52.js"
+  },
+  {
+    "revision": "e8c4aee828ca077205f5",
+    "url": "/codejam-group34/static/js/main.e8c4aee8.chunk.js"
+  },
+  {
+    "revision": "ba7912eacd1667ce3c61",
+    "url": "/codejam-group34/static/js/1.ba7912ea.chunk.js"
+  },
+  {
+    "revision": "e8c4aee828ca077205f5",
+    "url": "/codejam-group34/static/css/main.e65ca0ee.chunk.css"
+  },
+  {
+    "revision": "7d2504707ae4b44396739078167f7078",
+    "url": "/codejam-group34/index.html"
+  }
+];
